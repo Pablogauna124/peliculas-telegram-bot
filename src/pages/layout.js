f9fcb6ec19599323bfddc7447baa0a7cc5672270
Pablogauna124 <57366510+Pablogauna124@users.noreply.github.com>
2026-08-04 16:58:02 -0300

    Update layout.js

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -1,11 +1,4 @@
-function escapeHtml(value) {
-  return String(value ?? "")
-    .replaceAll("&", "&amp;")
-    .replaceAll("<", "&lt;")
-    .replaceAll(">", "&gt;")
-    .replaceAll('"', "&quot;")
-    .replaceAll("'", "&#039;");
-}
+import { escapeHtml } from "../utils/html.js";
 
 export function renderLayout(title, content) {
   return `<!DOCTYPE html>
